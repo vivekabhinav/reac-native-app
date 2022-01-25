0c67fba0c52c2e2ef60ec81aa4acffbeb75e1454
vivekabhinav <vivekabhinav7@gmail.com>
2022-01-25 12:44:55 +0530

    on change text

--- App.js
+++ App.js
@@ -5,18 +5,31 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
 
   const [name,setName] = useState('Abhinav')
-  const [person, setPerson] = useState({name: 'vivek', age: 21})
+  const [person, setPerson] = useState({name: 'Vivek', age: 21})
+
+  const [age,setAge] = useState('22')
+
 
   const clickHandler = () => {
-    setName('tripathi')
+    setName('Tripathi')
   }
 
   return (
     <View style={styles.container}>
       <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and his age is  {person.age}</Text>
-      <TextInput style={styles.input}
-      placeholder = 'enter name'/>
+      <Text>His name is {person.name} and his age is  {age}</Text>
+      <TextInput 
+      style={styles.input}
+      placeholder = 'enter name'
+      onChangeText = {(val) => setName(val)}
+      />
+
+      <TextInput 
+      keyboardType = 'numeric'
+      style={styles.input}
+      placeholder = 'enter age eg 88'
+      onChangeText = {(val) => setAge(val)}
+      />
       <Button title = 'update state'  onPress = {clickHandler}/>
     </View>
     
