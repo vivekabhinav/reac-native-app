887ab8d6772a505fbafd787844b2be47261a2273
vivekabhinav <vivekabhinav7@gmail.com>
2022-01-25 14:30:54 +0530

    components

--- App.js
+++ App.js
@@ -1,48 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import SectionListBasics from './components/SectionListBasics';
+import FlatListBasics from './components/FlatListBasics';
 
 export default function App() {
 
-  const [people, setPeople] = useState(
-    [
-      { name: 'Abhinav', key: '1' },
-      { name: 'Vivek', key: '2' },
-      { name: 'Vaibhav', key: '3' },
-      { name: 'Tanmaya', key: '4' },
-      { name: 'Muazzam', key: '5' },
-      { name: 'Abhi', key: '6' },
-      { name: 'Ani', key: '7' },
-      { name: 'Abhinav', key: '8' },
-      { name: 'Aniket', key: '9' },
-      { name: 'Harshit', key: '10' },
-      { name: 'Ankit', key: '11' },
-      { name: 'vivek', key: '12' },
-      { name: 'Abhinav', key: '13' },
-      { name: 'Niket', key: '14' },
-      { name: 'Aarshit', key: '15' },
-
-    ]
-  )
+ 
 
   return (
     <View style={styles.container}>
-    <ScrollView>
-      {
-        people.map((item) => {
-          return (
-            <View key={item.key}>
-              <Text style = {styles.item}>{item.name}</Text>
-            </View>
-          )
-        }
-        )}
-        </ScrollView>
+      <SectionListBasics/>
+      <FlatListBasics/>
 
     </View>
 
-  );
-}
+  );}
 
 const styles = StyleSheet.create({
   container: {
